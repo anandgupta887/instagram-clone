6d614e80c3bdb25af9e6e4ccbd5aa1dcadfebc69
Anand <anandgupta887@yahoo.com>
2020-08-21 16:11:01 +0530

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      <h1>hello world</h1>
+    </div>
+  );
 }
 
 export default App;
